88703fae680dd4036f143b50c717a21dbe57b8c5
Brorminskij <alexander.borminskij@gmail.com>
2021-02-20 17:39:45 +0100

    complete

---
+++ javascript.js
@@ -0,0 +1,29 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
+
+document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
+
+
+
+function playerWinner(){
+
+    if(randomNumber1 == randomNumber2){
+
+        document.querySelector("h1").innerHTML = "🎲Draw🎲";
+
+    } if(randomNumber1 > randomNumber2){
+
+        document.querySelector("h1").innerHTML = "🎲Player 1 Wins!!"
+
+    } else if(randomNumber2 > randomNumber1){
+
+        document.querySelector("h1").innerHTML = "Player 2 Wins!!🎲"
+
+    }
+
+}
+
+playerWinner();
